af1465f6710a893d30d1b8a1c8fcc73f0872867c
Shivam Mudgal <smudgal496@gmail.com>
2026-02-18 11:57:20 +0530

    Add function to find duplicate elements
    Implement function to find duplicate elements in an array.

---
+++ duplicateelements.py
@@ -0,0 +1,12 @@
+#Find Duplicate elements from an array
+def duplicates(arr):
+    n=len(arr)
+    c=[]
+    for i in range(n):
+        for j in range(i+1,n):
+            if arr[i]==arr[j]:
+                c.append(arr[i])
+    c=list(set(c))
+    return "duplicate elements=",c
+    
+print (duplicates([1,2,3,4,4,4,5,5,6,7,8,8]))
